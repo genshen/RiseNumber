1c6783a2b1ae09bb3f4ac4b6b88fa4fbe9e8d77f
ChuGenshen <genshen220284@foxmail.com>
2016-01-16 15:47:11 +0800

    add screenshot

--- app/src/main/java/com/holo/risenumber/MainActivity.java
+++ app/src/main/java/com/holo/risenumber/MainActivity.java
@@ -3,6 +3,8 @@ package com.holo.risenumber;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 import com.holo.risenumber.views.RiseNumberTextView;
 
@@ -15,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppCompatButton btn = (AppCompatButton)findViewById(R.id.start);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+
+    }
+
+    private void start() {
         // int rise
         rnt = (RiseNumberTextView) findViewById(R.id.rise);
         rnt.setRiseInterval(1, 99)
